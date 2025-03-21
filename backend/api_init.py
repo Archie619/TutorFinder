17f9b8b6f88c56e25d2871e886697252e8ea24f3
Nathan Snyder <ezspams56@gmail.com>
2025-03-21 11:19:39 -0500

    start working on class_posts

--- backend/api_init.py
+++ backend/api_init.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from .routers import login, classes, profile
+from .routers import login, classes, profile, class_posts
 
 app = FastAPI()
 
 app.include_router(login.router)
 app.include_router(profile.router)
 app.include_router(classes.router)
+app.include_router(class_posts.router)
 
 """ 
 Check if the backend opened up successfully
---
+++ backend/routers/class_posts.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter()
+
+########################################
+#           PYDANTIC MODELS            #
+########################################
+
+class ClassSpecification(BaseModel):
+    class_id: int
+
+class PostPreview(BaseModel):
+    pfp: str
+    name: str
+    rating: float | None
+
+class ClassPosts(BaseModel):
+    posts: list[PostPreview]
+
+class PostSpecification(BaseModel):
+    token: str
+    post_description: str
+
+class PostCreatedResponse:
+    valid: bool
+    errormsg: str | None
+
+########################################
+#             FUNCTIONS                #
+########################################
+
+'''
+Load a specific class
+'''
+@router.get('/class', response_model=ClassPosts)
+async def load_class(class_spec: ClassSpecification):
+    class_spec
+
+
+
+'''
+Create a post within a specific class
+'''
+@router.post('/class/create-post', response_model=PostCreatedResponse)
+async def create_post(post_spec: PostSpecification):
+    post_spec
